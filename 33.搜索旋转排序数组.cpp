6fbae6d599ef452f3c54d1d56b340a5d0056f541
Shawn <307882919@qq.com>
2019-04-11 22:15:47 +0800

    add #33

---
+++ 33.搜索旋转排序数组.cpp
@@ -0,0 +1,47 @@
+/*
+ * @lc app=leetcode.cn id=33 lang=cpp
+ *
+ * [33] 搜索旋转排序数组
+ *
+ * https://leetcode-cn.com/problems/search-in-rotated-sorted-array/description/
+ *
+ * algorithms
+ * Medium (36.33%)
+ * Total Accepted:    20.4K
+ * Total Submissions: 56.3K
+ * Testcase Example:  '[4,5,6,7,0,1,2]\n0'
+ *
+ * 假设按照升序排序的数组在预先未知的某个点上进行了旋转。
+ * 
+ * ( 例如，数组 [0,1,2,4,5,6,7] 可能变为 [4,5,6,7,0,1,2] )。
+ * 
+ * 搜索一个给定的目标值，如果数组中存在这个目标值，则返回它的索引，否则返回 -1 。
+ * 
+ * 你可以假设数组中不存在重复的元素。
+ * 
+ * 你的算法时间复杂度必须是 O(log n) 级别。
+ * 
+ * 示例 1:
+ * 
+ * 输入: nums = [4,5,6,7,0,1,2], target = 0
+ * 输出: 4
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: nums = [4,5,6,7,0,1,2], target = 3
+ * 输出: -1
+ * 
+ */
+
+// 这种题的意义在哪里？？？
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        for(int i = 0; i < nums.size(); i++){ 
+            if(nums[i]==target) return i; 
+        } 
+        return -1; 
+    }
+};
+
