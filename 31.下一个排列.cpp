f1528267cf2bff1b334da3305b11985b91404c5f
Shawn <307882919@qq.com>
2019-04-11 21:05:58 +0800

    add #31

---
+++ 31.下一个排列.cpp
@@ -0,0 +1,57 @@
+/*
+ * @lc app=leetcode.cn id=31 lang=cpp
+ *
+ * [31] 下一个排列
+ *
+ * https://leetcode-cn.com/problems/next-permutation/description/
+ *
+ * algorithms
+ * Medium (30.53%)
+ * Total Accepted:    10.8K
+ * Total Submissions: 35.3K
+ * Testcase Example:  '[1,2,3]'
+ *
+ * 实现获取下一个排列的函数，算法需要将给定数字序列重新排列成字典序中下一个更大的排列。
+ * 
+ * 如果不存在下一个更大的排列，则将数字重新排列成最小的排列（即升序排列）。
+ * 
+ * 必须原地修改，只允许使用额外常数空间。
+ * 
+ * 以下是一些例子，输入位于左侧列，其相应输出位于右侧列。
+ * 1,2,3 → 1,3,2
+ * 3,2,1 → 1,2,3
+ * 1,1,5 → 1,5,1
+ * 
+ */
+
+/* 太难了，题都读不懂
+According to Wikipedia, a man named Narayana Pandita presented the following simple algorithm to solve this problem in the 14th century.
+
+1. Find the largest index k such that nums[k] < nums[k + 1]. If no such index exists, just reverse nums and done.
+2. Find the largest index l > k such that nums[k] < nums[l].
+3. Swap nums[k] and nums[l].
+4. Reverse the sub-array nums[k + 1:].
+*/
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size(), k, l;
+        for (k = n - 2; k >= 0; --k) {
+            if (nums[k] < nums[k + 1]) {
+                break;
+            }
+        }
+        if (k < 0) {
+            reverse(nums.begin(), nums.end());
+        } else {
+            for (l = n - 1; l > k; --l){
+                if (nums[l] > nums[k]) {
+                    break;
+                }
+            }
+            swap(nums[k], nums[l]);
+            reverse(nums.begin() + k + 1, nums.end());
+        }
+    }
+};
+
