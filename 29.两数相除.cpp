69eaa79061c1dbb0e8e404d70c7e5fa7cbf1986d
Shawn <307882919@qq.com>
2019-04-11 09:58:56 +0800

    add #29

---
+++ 29.两数相除.cpp
@@ -0,0 +1,59 @@
+/*
+ * @lc app=leetcode.cn id=29 lang=cpp
+ *
+ * [29] 两数相除
+ *
+ * https://leetcode-cn.com/problems/divide-two-integers/description/
+ *
+ * algorithms
+ * Medium (17.85%)
+ * Total Accepted:    10.6K
+ * Total Submissions: 59.6K
+ * Testcase Example:  '10\n3'
+ *
+ * 给定两个整数，被除数 dividend 和除数 divisor。将两数相除，要求不使用乘法、除法和 mod 运算符。
+ * 
+ * 返回被除数 dividend 除以除数 divisor 得到的商。
+ * 
+ * 示例 1:
+ * 
+ * 输入: dividend = 10, divisor = 3
+ * 输出: 3
+ * 
+ * 示例 2:
+ * 
+ * 输入: dividend = 7, divisor = -3
+ * 输出: -2
+ * 
+ * 说明:
+ * 
+ * 
+ * 被除数和除数均为 32 位有符号整数。
+ * 除数不为 0。
+ * 假设我们的环境只能存储 32 位有符号整数，其数值范围是 [−2^31,  2^31 − 1]。本题中，如果除法结果溢出，则返回 2^31 − 1。
+ * 
+ * 
+ */
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        if (needle.size() == 0) return 0;
+        if (haystack.size() < needle.size()) return -1;
+        int j = 0;
+        int i = 0;
+        for (; i < haystack.size(); ++i) {
+            if (j == needle.size()) return i - needle.size();
+            if (haystack[i] == needle[j]) {
+                ++j;
+            } else {
+                i -= j;
+                j = 0;
+            }
+        }
+        if (j == needle.size()) {
+            return i - needle.size();
+        }
+        return -1;
+    }
+};
+
