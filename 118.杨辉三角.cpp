5bf7e5cd233d43724f27f9427570d0b2df97980d
Shawn <307882919@qq.com>
2019-05-13 22:10:37 +0800

    add #119

--- 118.杨辉三角.cpp
+++ 118.杨辉三角.cpp
@@ -6,7 +6,21 @@
 给定一个非负整数 numRows，生成杨辉三角的前 numRows 行。
 在杨辉三角中，每个数是它左上方和右上方的数的和。
 
+示例:
+
+输入: 5
+输出:
+[
+     [1],
+    [1,1],
+   [1,2,1],
+  [1,3,3,1],
+ [1,4,6,4,1]
+]
+
  */
+
+// 方法一：
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
@@ -25,4 +39,23 @@ public:
         return res;
     }
 };
+
+// 方法二：
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> res;
+        vector<int> row;
+        for (int i = 0; i < numRows; i++) {
+            row.push_back(1);
+            for (int j = i - 1; j > 0; j--) {
+                row[j] = row[j - 1] + row[j];
+            }
+            res.push_back(row);
+        }
+        return res;
+    }
+};
+
+
 
---
+++ 119.杨辉三角-ii.cpp
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=119 lang=cpp
+ *
+ * [119] 杨辉三角 II
+
+在杨辉三角中，每个数是它左上方和右上方的数的和。
+
+示例:
+
+输入: 3
+输出: [1,3,3,1]
+进阶：
+
+你可以优化你的算法到 O(k) 空间复杂度吗？
+
+ */
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        vector<int> vec;
+        for (int i = 0; i < rowIndex + 1; i++) {
+            vec.push_back(1);
+            for (int j=i-1; j >= 1; j--) {
+                vec[j] = vec[j-1] + vec[j];
+            }
+        }
+        return vec;
+    }
+};
+
