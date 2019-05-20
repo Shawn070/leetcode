8973b3e090d42db9483b8b2b53d779df52a0ecc9
Shawn <307882919@qq.com>
2019-05-20 11:43:03 +0800

    add #706

---
+++ 599.两个列表的最小索引总和.cpp
@@ -0,0 +1,12 @@
+/*
+ * @lc app=leetcode.cn id=599 lang=cpp
+ *
+ * [599] 两个列表的最小索引总和
+ */
+class Solution {
+public:
+    vector<string> findRestaurant(vector<string>& list1, vector<string>& list2) {
+        
+    }
+};
+
