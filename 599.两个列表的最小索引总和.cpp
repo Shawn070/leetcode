df811c096592919cc02373d657ce1746c27be7c8
Shawn <307882919@qq.com>
2019-05-20 15:17:13 +0800

    add #559

--- 599.两个列表的最小索引总和.cpp
+++ 599.两个列表的最小索引总和.cpp
@@ -2,11 +2,53 @@
  * @lc app=leetcode.cn id=599 lang=cpp
  *
  * [599] 两个列表的最小索引总和
+
+假设Andy和Doris想在晚餐时选择一家餐厅，并且他们都有一个表示最喜爱餐厅的列表，每个餐厅的名字用字符串表示。
+
+你需要帮助他们用最少的索引和找出他们共同喜爱的餐厅。 如果答案不止一个，则输出所有答案并且不考虑顺序。 你可以假设总是存在一个答案。
+
+示例 1:
+
+输入:
+["Shogun", "Tapioca Express", "Burger King", "KFC"]
+["Piatti", "The Grill at Torrey Pines", "Hungry Hunter Steakhouse", "Shogun"]
+输出: ["Shogun"]
+解释: 他们唯一共同喜爱的餐厅是“Shogun”。
+示例 2:
+
+输入:
+["Shogun", "Tapioca Express", "Burger King", "KFC"]
+["KFC", "Shogun", "Burger King"]
+输出: ["Shogun"]
+解释: 他们共同喜爱且具有最小索引和的餐厅是“Shogun”，它有最小的索引和1(0+1)。
+提示:
+
+两个列表的长度范围都在 [1, 1000]内。
+两个列表中的字符串的长度将在[1，30]的范围内。
+下标从0开始，到列表的长度减1。
+两个列表都没有重复的元素。
+
  */
 class Solution {
 public:
     vector<string> findRestaurant(vector<string>& list1, vector<string>& list2) {
-        
+        vector<string> res;
+        unordered_map<string, int> um;
+        int min = INT_MAX;
+        for (int i=0; i < list1.size(); i++)
+            um[list1[i]] = i;
+        for (int j=0; j < list2.size(); j++) {
+            if (um.count(list2[j])) {   //找到相同string
+                if (um[list2[j]] + j < min)  {  //索引和不为最小
+                    min = um[list2[j]] + j;
+                    res.clear();
+                    res.push_back(list2[j]);
+                } else if (um[list2[j]] + j == min) {   //索引和为最小
+                    res.push_back(list2[j]);
+                }
+            }
+        }
+        return res;
     }
 };
 
