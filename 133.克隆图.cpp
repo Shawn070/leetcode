c85d37ed1e514229801ecf4843a9c753d05214b5
Shawn <307882919@qq.com>
2019-05-27 18:27:51 +0800

    add #133

--- 133.克隆图.cpp
+++ 133.克隆图.cpp
@@ -45,10 +45,52 @@ public:
     }
 };
 */
+
+//DFS
 class Solution {
 public:
     Node* cloneGraph(Node* node) {
-        
+        if (!node) {
+            return NULL;
+        }
+        if (copies.find(node) == copies.end()) {//确保该图为连通图
+            copies[node] = new Node(node->val, {});
+            for (Node* neighbor : node->neighbors) {
+                copies[node] -> neighbors.push_back(cloneGraph(neighbor));
+            }
+        }
+        return copies[node];
     }
+    private:
+    unordered_map<Node *, Node *> copies; // <curNode, nextNode>
 };
+
+//BFS(未看)
+class Solution {
+public:
+    Node* cloneGraph(Node* node) {
+        if (!node) {
+            return NULL;
+        }
+        Node* copy = new Node(node -> val, {});
+        copies[node] = copy;
+        queue<Node*> todo;
+        todo.push(node);
+        while (!todo.empty()) {
+            Node* cur = todo.front();
+            todo.pop();
+            for (Node* neighbor : cur -> neighbors) {
+                if (copies.find(neighbor) == copies.end()) {
+                    copies[neighbor] = new Node(neighbor -> val, {});
+                    todo.push(neighbor);
+                }
+                copies[cur] -> neighbors.push_back(copies[neighbor]);
+            }
+        }
+        return copy;
+    }
+private:
+    unordered_map<Node*, Node*> copies;
+};
+
 
