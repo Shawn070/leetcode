6196c5f3e66852bf06a1dbb14c7bac4849c03dda
Shawn <307882919@qq.com>
2019-06-01 09:39:57 +0800

    update #93

--- 93.复原ip地址.cpp
+++ 93.复原ip地址.cpp
@@ -28,14 +28,14 @@ public:
             res.push_back(ip);
             return;
         }
-        //剩余元素过多或过少
-        if (s.size() - start > (4 - step) * 3) return ;
-        if (s.size() - start < (4 - step)) return ;
+        //剩余元素过多或过少，则提前返回退出，不满足“深度到达”要求
+        if (s.size() - start > (4 - step) * 3) return ; //后面每层最多3个数字
+        if (s.size() - start < (4 - step)) return ;     //后面每层最少1个数字
         //遍历每层中的每个数
         int num = 0;    //每层中的数字
         for (int i = start; i < start + 3; i++) {
             num = num * 10 + (s[i] - '0');
-            if (num <= 255) {
+            if (num <= 255) {   //满足条件就深度遍历
                 ip += s[i];
                 DFS(s, i + 1, step + 1, ip + ".", res);
             }
