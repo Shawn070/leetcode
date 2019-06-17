be981f04eaec5a87800c9d3e492f62737c76e584
Shawn <307882919@qq.com>
2019-06-17 15:59:14 +0800

    add #203

---
+++ 203.移除链表元素.cpp
@@ -0,0 +1,45 @@
+/*
+ * @lc app=leetcode.cn id=203 lang=cpp
+ *
+ * [203] 移除链表元素
+
+删除链表中等于给定值 val 的所有节点。
+
+示例:
+
+输入: 1->2->6->3->4->5->6, val = 6
+输出: 1->2->3->4->5
+
+ */
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        ListNode *root = head;
+        ListNode *pre;
+        while(head) {
+            if (head->val == val) {
+                if (head == root) {
+                    head = head->next;
+                    root = head;
+                    continue;
+                } else {
+                    pre->next = head->next;
+                    head = pre->next;
+                    continue;
+                }
+            }
+            pre  = head;
+            head = head->next;
+        }
+        return root;
+    }
+};
+
