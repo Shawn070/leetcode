6ca2e2276d9f400d9e520eb9a5fc4d0589cfaaa3
Shawn <307882919@qq.com>
2019-06-27 20:19:12 +0800

    update #21

--- 21.合并两个有序链表.cpp
+++ 21.合并两个有序链表.cpp
@@ -36,6 +36,12 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+
+// 遍历：（28 ms	8.8 MB）
+/*
+时间复杂度：O(n+m)
+空间复杂度：O(1)
+*/
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
@@ -61,5 +67,25 @@ public:
             res = res->next;
         }
         return head->next; //略过0开头节点
+    }
+};
+
+// 递归:(12 ms	9.2 MB)
+/*
+时间复杂度：O(n+m)
+空间复杂度：O(n+m), n+m 的栈将会消耗 O(n+m) 的空间
+*/
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if (!l1) return l2;
+        else if (!l2) return l1;
+        else if (l1->val < l2->val) {
+            l1->next = mergeTwoLists(l1->next, l2);
+            return l1;
+        } else {
+            l2->next = mergeTwoLists(l2->next, l1);
+            return l2;
+        }
     }
 };
