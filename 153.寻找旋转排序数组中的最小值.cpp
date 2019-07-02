70103b0c1787fcfd3f78a8fd79076ccceb61da09
Shawn <307882919@qq.com>
2019-07-02 17:00:47 +0800

    add #153

---
+++ 153.寻找旋转排序数组中的最小值.cpp
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode.cn id=153 lang=cpp
+ *
+ * [153] 寻找旋转排序数组中的最小值
+
+假设按照升序排序的数组在预先未知的某个点上进行了旋转。
+
+( 例如，数组 [0,1,2,4,5,6,7] 可能变为 [4,5,6,7,0,1,2] )。
+
+请找出其中最小的元素。
+
+你可以假设数组中不存在重复元素。
+
+示例 1:
+
+输入: [3,4,5,1,2]
+输出: 1
+示例 2:
+
+输入: [4,5,6,7,0,1,2]
+输出: 0
+
+ */
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int l = 0, r = nums.size() - 1;
+        if (nums[l] <= nums[r]) return nums[l];
+        int mid;
+        while (l < r && r - l > 1) {
+            mid = l + (r - l) / 2;
+            if (nums[mid] > nums[l])
+                l = mid;
+            else 
+                r = mid;
+        }
+        return min(nums[l], nums[r]);
+    }
+};
+
