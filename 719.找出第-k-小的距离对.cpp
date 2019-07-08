63620969a41d1ab6b2dace27f78a35b11dbb8378
Shawn-PC <307882919@qq.com>
2019-07-08 20:36:50 +0800

    update for temp

---
+++ 719.找出第-k-小的距离对.cpp
@@ -0,0 +1,12 @@
+/*
+ * @lc app=leetcode.cn id=719 lang=cpp
+ *
+ * [719] 找出第 k 小的距离对
+ */
+class Solution {
+public:
+    int smallestDistancePair(vector<int>& nums, int k) {
+        
+    }
+};
+
