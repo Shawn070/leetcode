00e2b51c4bff49658876bd86062aaa7fa22bb2c5
Shawn-PC <307882919@qq.com>
2019-08-28 22:27:53 +0800

    update for 归并排序

---
+++ 归并排序/排序数组.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+
+using namespace std;
+
+void Merge(int* L, int* A, int lenA, int* B, int lenB) {
+    int i = 0, j = 0, k = 0;
+    while (i < lenA && j < lenB) {
+        if (A[i] < B[j])
+            L[k++] = A[i++];
+        else
+            L[k++] = B[j++];
+    }
+    while (i < lenA)
+        L[k++] = A[i++];
+    while (j < lenB)
+        L[k++] = B[j++];
+}
+
+void MergeSort(int* L, int len) {
+    if (len < 2)
+        return ;
+    int mid = len / 2;
+    int* A = new int[mid];
+    int* B = new int[len - mid];
+    for (int i = 0; i < mid; i++)
+        A[i] = L[i];
+    for (int j = mid; j < len; j++)
+        B[j - mid] = L[j];
+    MergeSort(A, mid);
+    MergeSort(B, len - mid);
+    Merge(L, A, mid, B, len - mid);
+    delete []A;
+    delete []B;
+}
+
+int main(){
+    int L[9] = {8, 6, 7, 9, 1, 3, 2, 5, 4};
+    int len = sizeof(L) / sizeof(L[0]);
+    MergeSort(L, len);
+    for (auto L : L) {
+        cout << L << endl;
+    }
+    cin >> len;
+    return 0;
+}
